db4bf0c8881a0317cb0dd7c7730f281bb0088384
Tjandra Putra <tjandrap.work@gmail.com>
2023-09-04 18:16:07 +0800

    routing and packages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
-import logo from "./logo.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// user routes
+import { Navbar, Footer, Home, Login, Register } from "./routes/Routes";
+
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* Static content goes here */}
+      <Navbar />
+
+      {/* Dynamic content goes here */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+
+      {/* Static content goes here */}
+      <Footer />
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/routes/Routes.js
@@ -0,0 +1,7 @@
+import Navbar from "../components/Layout/Navbar/Navbar";
+import Footer from "../components/Layout/Footer/Footer";
+import Home from "../pages/Home/Home";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
+
+export { Navbar, Footer, Home, Login, Register };
