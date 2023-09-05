3f7d6248d41b45c12a78001cc7f31ffdbc9a1902
Tjandra Putra <tjandrap.work@gmail.com>
2023-09-05 16:55:30 +0800

    sidebar iter 1

--- frontend/src/routes/Routes.js
+++ frontend/src/routes/Routes.js
@@ -3,5 +3,6 @@ import Footer from "../components/Layout/Footer/Footer";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
 import Register from "../pages/Register/Register";
+import Sidebar from "../components/Layout/Sidebar/Sidebar";
 
-export { Navbar, Footer, Home, Login, Register };
+export { Navbar, Footer, Home, Login, Register, Sidebar };
