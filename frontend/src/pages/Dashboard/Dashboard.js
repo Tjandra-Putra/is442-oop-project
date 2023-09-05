2f730732ae13830256aa647baa94c5fbecfdce6b
Ming Jun Toh <iammingjun99@gmail.com>
2023-09-05 16:57:18 +0800

    added dashboard folder

---
+++ frontend/src/pages/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Chart from 'chart.js/auto'
+
+const Dashboard = () => {
+
+    return (
+        <div>
+        <h1>Dashboard</h1>
+        </div>
+        );
+ }
+
+ export default Dashboard;
