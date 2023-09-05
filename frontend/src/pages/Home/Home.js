e3ead60f1630db9eca8db5e45841190ffb4a27f2
Ming Jun Toh <iammingjun99@gmail.com>
2023-09-05 21:38:05 +0800

    coding during ethics class sucks

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Container from "@mui/material/Container";
+// import Container from "@mui/material/Container";
+import styles from "./Home.module.css";
+
 
 const home = () => {
   return (
-    <Container>
-      <h1>Home</h1>
-    </Container>
+
+    <>
+    
+    {/* First image */}
+    <div className={styles.stockone}>
+      <p>Welcome to Investment portfolio</p>
+      <p className={styles.landingpage} > Start Building your Investment Portfolio Today</p>
+    </div>
+
+      
+    {/* Second image */}
+    <div className={styles.stocktwo}>
+      <img src="image7.jpg"></img>
+    </div>
+      </>
+
   );
 };
 
