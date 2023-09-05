0a74e5ad7b03e263e00f4599054e2cba961c63f4
Ming Jun Toh <iammingjun99@gmail.com>
2023-09-06 02:03:18 +0800

    added the images but unable to fix responsiveness

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -8,18 +8,35 @@ const home = () => {
 
     <>
     
-    {/* First image */}
-    <div className={styles.stockone}>
-      <p>Welcome to Investment portfolio</p>
-      <p className={styles.landingpage} > Start Building your Investment Portfolio Today</p>
+    {/* First Section */}
+    <div className={styles.gridcontainerone}>
+      <div className={styles.stockone}>
+        <p>Welcome to Investment portfolio</p>
+        <p className={styles.landingpage} > Start Building your Investment Portfolio Today</p>
+      </div>
+    </div>
+      
+  
+    {/* Second Section - Grid with two columns */}
+    <div className={styles.gridcontainertwo}>
+      <div className={styles.stocktwo}>
+      <img src="image7.jpg"></img>
+      </div>
+      <div className={styles.stocktwoitem}>
+        <p>Stock Market is a web application that allows users to view stock prices and company information. Users can search for stocks by company name or ticker symbol, and view the latest price data for each stock.</p>
+      </div>
     </div>
 
+    {/* Third Section - Grid with two columns */}
+    <div className={styles.gridcontainerthree}>
+      <p>Stock Market is a web application that allows users to view stock prices and company information. Users can search for stocks by company name or ticker symbol, and view the latest price data for each stock.</p>
       
-    {/* Second image */}
-    <div className={styles.stocktwo}>
+      <div className={styles.stockthree}>
       <img src="image7.jpg"></img>
+      </div>
     </div>
-      </>
+
+    </>
 
   );
 };
