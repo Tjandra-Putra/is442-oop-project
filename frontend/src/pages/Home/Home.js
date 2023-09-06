9e5f98350312b50f6bdc15b82ecc1f0ba5093fa8
Ming Jun Toh <iammingjun99@gmail.com>
2023-09-06 16:19:18 +0800

    added some code to test

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -10,17 +10,19 @@ const home = () => {
     
     {/* First Section */}
     <div className={styles.gridcontainerone}>
-      <div className={styles.stockone}>
+      <div className={styles.stockimageone}>
         <p>Welcome to Investment portfolio</p>
         <p className={styles.landingpage} > Start Building your Investment Portfolio Today</p>
       </div>
     </div>
       
+
+    <div className={styles.emptydiv} ></div>
   
     {/* Second Section - Grid with two columns */}
     <div className={styles.gridcontainertwo}>
-      <div className={styles.stocktwo}>
-      <img src="image7.jpg"></img>
+      <div className={styles.stockimagetwo}>
+        <img src="image7.jpg"></img>
       </div>
       <div className={styles.stocktwoitem}>
         <p>Stock Market is a web application that allows users to view stock prices and company information. Users can search for stocks by company name or ticker symbol, and view the latest price data for each stock.</p>
@@ -29,10 +31,11 @@ const home = () => {
 
     {/* Third Section - Grid with two columns */}
     <div className={styles.gridcontainerthree}>
+      <div>
       <p>Stock Market is a web application that allows users to view stock prices and company information. Users can search for stocks by company name or ticker symbol, and view the latest price data for each stock.</p>
-      
-      <div className={styles.stockthree}>
-      <img src="image7.jpg"></img>
+      </div>
+      <div className={styles.stockimagethree}>
+        <img src="image7.jpg"></img>
       </div>
     </div>
 
