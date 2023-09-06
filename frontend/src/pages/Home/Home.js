a94eb0d535295d1dd6092147f6183ed15e751596
Ming Jun Toh <iammingjun99@gmail.com>
2023-09-07 01:04:41 +0800

    done for the day

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -6,16 +6,48 @@ const home = () => {
   return (
     <div className={style.homeWrapper}>
 
-        {/* First div */}
+        {/* First div - Created a grid of 4 columns with the fonts occupying the second columm */}
         <div className={style.firstWrapper}>
+          <div></div>
             <div className={style.firstWrappertext}>
-                <p id = {style.FirstWrappertext}>Welcome to Goldman Sachs</p>
-                <p>Here you can find good financial products</p>
+                <div className={style.landingPage} >
+                   <p>Welcome to Goldman Sachs</p>
+                    <p>Here you can find good financial products</p>
+                </div>
             </div>
+          <div></div>
+          <div></div>
         </div>
 
         {/* Second div */}
         <div className={style.emptydiv} ></div>
+
+        {/* Third div */}
+        <div className={style.secondWrapper}>
+          {/* First Row */}
+          <div className={style.secondWrapperImage}>
+            <img src="image7.jpg"></img>
+          </div>
+          {/* Second Row */}
+          <div>
+          <p>Welcome to Goldman Sachs</p>
+          <p>Here you can find good financial products</p>
+          </div>
+        </div>
+
+        {/* Third div */}
+        <div className={style.thirdWrapper}>
+          {/* First Row */}
+          <div className={style.thirdWrapperImage}>
+            <p>Welcome to Goldman Sachs</p>
+            <p>Here you can find good financial products</p>
+          </div>
+          {/* Second Row */}
+          <div>
+          <img src="image7.jpg"></img>
+          </div>
+        </div>
+
 
     </div>
   );
