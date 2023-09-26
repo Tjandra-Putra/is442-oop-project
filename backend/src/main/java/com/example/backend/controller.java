1aaaa02d276115f24fadd8edc382b1729497c387
gcshane <145309552+gcshane-smu@users.noreply.github.com>
2023-09-26 15:48:57 +0800

    Trying out some backend route

--- backend/src/main/java/com/example/backend/controller.java
+++ backend/src/main/java/com/example/backend/controller.java
@@ -1,6 +1,9 @@
 package com.example.backend;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
@@ -10,5 +13,13 @@ public class controller {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @GetMapping("/user")
+    public ArrayList<String> users() {
+        ArrayList<String> userList = new ArrayList<>();
+        userList.add("User1");
+        userList.add("User2");
+        return userList;
     }
 }
