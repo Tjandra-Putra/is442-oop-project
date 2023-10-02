7149fdeec46b0606b416707c56331f237f93c1b0
ryanteojw <ryanteo_99@hotmail.com>
2023-10-02 21:04:32 +0800

    Remove extra line in BackendApplication.java

--- backend/src/main/java/gs/BackendApplication.java
+++ backend/src/main/java/gs/BackendApplication.java
@@ -6,8 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@ComponentScan(
-		basePackages = {"gs.controller", "gs.model", "gs.repository", "gs.service", "gs.config"})
 @ComponentScan(basePackages = {"gs.controller", "gs.model", "gs.repository", "gs.service", "gs.config"})
 @RestController
 public class BackendApplication {
