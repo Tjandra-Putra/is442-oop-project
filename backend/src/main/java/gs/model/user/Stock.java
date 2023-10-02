61b3208e8eff0dab1450e35273bd86f803ba05d7
gcshane <145309552+gcshane-smu@users.noreply.github.com>
2023-10-02 15:45:52 +0800

    Stock db model

---
+++ backend/src/main/java/gs/model/user/Stock.java
@@ -0,0 +1,69 @@
+package gs.model.user;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "stock")
+
+public class Stock {
+    
+    @Id
+    @Column(name = "ticker")
+
+    private String ticker;
+    private String industry;
+    private String sector;
+    private String country;
+    private String name;
+
+    public Stock(String ticker, String industry, String sector, String country, String name) {
+        this.ticker = ticker;
+        this.industry = industry;
+        this.sector = sector;
+        this.country = country;
+        this.name = name;
+    }
+
+    public String getTicker() {
+        return this.ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getIndustry() {
+        return this.industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getSector() {
+        return this.sector;
+    }
+
+    public void setSector(String sector) {
+        this.sector = sector;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
