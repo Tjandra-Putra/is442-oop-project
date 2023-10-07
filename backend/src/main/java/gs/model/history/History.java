a8bdc27b84bb08aeb392914e675f2ab68f4880ee
Faez <faezlatiff0706@gmail.com>
2023-10-07 12:13:49 +0800

    Added base History model

---
+++ backend/src/main/java/gs/model/history/History.java
@@ -0,0 +1,49 @@
+package gs.model.history;
+
+import java.util.Date;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "history")
+
+public class History{
+    @Id
+    private String ticker;
+
+    @Id
+    private Date date;
+    private double adjClosePrice;
+
+    public History(String ticker, Date date, double adjClosePrice) {
+        this.ticker = ticker;
+        this.date = date;
+        this.adjClosePrice = adjClosePrice;
+    }   
+    
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public double getAdjClosePrice() {
+        return adjClosePrice;
+    }
+
+    public void setAdjClosePrice(double adjClosePrice){
+        this.adjClosePrice = adjClosePrice;
+    }
+
+}
