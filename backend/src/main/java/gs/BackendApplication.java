39154a0dce1272aadf675765f1563668da0ef5e2
gcshane <145309552+gcshane-smu@users.noreply.github.com>
2023-10-18 15:47:58 +0800

    Populate Stock table on Spring Boot app start

--- backend/src/main/java/gs/BackendApplication.java
+++ backend/src/main/java/gs/BackendApplication.java
@@ -1,17 +1,38 @@
 package gs;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RestController;
 
+import cronjob.stockAPI;
+import java.util.Map;
+
+import gs.repository.StockRepo;
+import gs.service.stock.StockService;
+import gs.service.stock.StockServiceImpl;
+import jakarta.annotation.Resource;
+import gs.repository.StockRepo;
+
 @SpringBootApplication
 @ComponentScan(basePackages = {"gs.controller", "gs.entity", "gs.repository", "gs.service", "gs.config"})
 @RestController
-public class BackendApplication {
+public class BackendApplication implements CommandLineRunner {
+
+	@Autowired
+	protected StockService stockService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		stockAPI stocks = new stockAPI();
+		Map<String,String> allStocks = stocks.getAllStocks();
+		allStocks.forEach((ticker, name) -> stockService.addStock(ticker, name));
 	}
 
 }
