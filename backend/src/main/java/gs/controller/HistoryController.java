6052c539956c2367aea3d57413d49ec2b4b94455
Ming Jun Toh <iammingjun99@gmail.com>
2023-10-30 18:48:52 +0800

    added the method to add the weeklyprice to db

--- backend/src/main/java/gs/controller/HistoryController.java
+++ backend/src/main/java/gs/controller/HistoryController.java
@@ -38,4 +38,11 @@ public class HistoryController {
     ){
         return ApiModel.ok(HistoryService.getHistoryByTicker(ticker));
     }
+
+     @GetMapping("/getHistoryByWeekTicker/{ticker}")
+    public ApiModel<ArrayList<HistoryInputModel>> updateWeeklyHistory (
+        @PathVariable("ticker") String ticker
+    ){
+        return ApiModel.ok(HistoryService.updateWeeklyHistory(ticker));
+    }
 }
