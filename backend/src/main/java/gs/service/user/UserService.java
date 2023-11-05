e019177c0f92368c99604afc8632ca3170395e56
Alexis-SLT <alexisseow99@gmail.com>
2023-11-06 01:14:33 +0800

    Change Password Update

--- backend/src/main/java/gs/service/user/UserService.java
+++ backend/src/main/java/gs/service/user/UserService.java
@@ -1,9 +1,11 @@
 package gs.service.user;
 
+import java.security.Principal;
 import java.util.List;
 
 import gs.common.ApiModel;
 import gs.common.RequestModel;
+import gs.entity.ChangePasswordRequest;
 import gs.entity.User;
 import gs.inputModel.UserInputModel;
 import jakarta.servlet.http.HttpServletResponse;
@@ -14,4 +16,6 @@ public interface UserService {
     List<UserInputModel> getUserById(String id);
 
     ApiModel addUser(HttpServletResponse response, RequestModel requestModel, ApiModel apiModel) throws Exception;
+
+    void changePassword(ChangePasswordRequest request, Principal connectedUser);
 }
