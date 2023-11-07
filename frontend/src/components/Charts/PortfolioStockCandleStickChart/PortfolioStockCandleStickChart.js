5e86fa979d5ac7d305adf25f8cc9365614c78c36
Tjandra Putra <tjandrap.work@gmail.com>
2023-11-07 23:04:49 +0800

    Update PortfolioStockCandleStickChart.js

--- frontend/src/components/Charts/PortfolioStockCandleStickChart/PortfolioStockCandleStickChart.js
+++ frontend/src/components/Charts/PortfolioStockCandleStickChart/PortfolioStockCandleStickChart.js
@@ -1,48 +1,65 @@
 import React, { useEffect, useState } from "react";
 import { Chart } from "react-google-charts";
 import axios from "axios";
-import { Typography } from "@mui/material/";
+import { Typography, Select, MenuItem } from "@mui/material/";
 
 const PortfolioStockCandleStickChart = React.memo(({ ticker }) => {
-  const [initialData, setInitialData] = useState([
-    ["date", "openPrice", "lowPrice", "highPrice", "adjustedClosingPrice"],
-    ["1999-11-12 00:00:00.0", 87.75, 86.75, 97.73, 0.6867],
-  ]);
+  // const [initialData, setInitialData] = useState([
+  // adjustprice is closingPrice
+  //   ["date", "openPrice", "lowPrice", "highPrice", "adjustedClosingPrice"],
+  //   // ["1999-11-12 00:00:00.0", 87.75, 86.75, 97.73, 0.6867],
+  // ]);
+  const [initialData, setInitialData] = useState([["date", "openPrice", "closePrice", "lowPrice", "highPrice"]]);
+
+  const [showAllData, setShowAllData] = useState(0); // [true, false
+  const [dataSetsToShow, setDataSetsToShow] = useState(10);
 
   useEffect(() => {
     axios
       .get(`http://localhost:8080/api/stockHistory/getHistoryByWeekTicker/${ticker}`)
       .then((res) => {
-        const responseData = res.data.data;
+        let responseData = res.data.data; // Define responseData here
+        responseData = responseData.reverse(); // Reverse the data so that the most recent data is at the end
 
-        // Map the received data to match the structure of initialData
-        const formattedData = responseData.map((item) => [
-          item.date, // date
-          item.openPrice, // openPrice
-          item.lowPrice, // lowPrice
-          item.highPrice, // highPrice
-          item.adjClosePrice, // adjustedClosingPrice
-        ]);
+        setShowAllData(responseData.length);
 
-        // Add the header row to the formatted data
-        formattedData.unshift(["date", "openPrice", "lowPrice", "highPrice", "adjustedClosingPrice"]);
+        const formattedData = [];
+        for (let i = 0; i < responseData.length && i < dataSetsToShow; i++) {
+          const item = responseData[i];
+          formattedData.push([item.date, item.openPrice, item.adjClosePrice, item.lowPrice, item.highPrice]);
+        }
 
+        formattedData.unshift(["date", "openPrice", "closePrice", "lowPrice", "highPrice"]);
         setInitialData(formattedData);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
-  }, [ticker]);
+  }, [ticker, dataSetsToShow]);
 
   const options = {
     legend: "none",
   };
 
+  const handleDataSetsChange = (event) => {
+    setDataSetsToShow(event.target.value);
+  };
+
   return (
     <>
-      <Typography id="modal-modal-title" variant="h6" component="h2">
-        Stock Name: {ticker}
-      </Typography>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Stock Name: {ticker}
+        </Typography>
+        <Select value={dataSetsToShow} onChange={handleDataSetsChange}>
+          <MenuItem value={10}>Show 10 Data Sets</MenuItem>
+          <MenuItem value={20}>Show 20 Data Sets</MenuItem>
+          <MenuItem value={30}>Show 30 Data Sets</MenuItem>
+          <MenuItem value={40}>Show 40 Data Sets</MenuItem>
+          <MenuItem value={50}>Show 50 Data Sets</MenuItem>
+          <MenuItem value={showAllData}>Show All Data Sets</MenuItem>
+        </Select>
+      </div>
       <Chart chartType="CandlestickChart" width="100%" height="400px" data={initialData} options={options} />
     </>
   );
